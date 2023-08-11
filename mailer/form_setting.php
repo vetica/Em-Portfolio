1bffba8df223fe453126317f855146f02d81e9b1
vetica <sholladay@gannett.com>
2023-08-11 19:21:36 -0400

    changes file structure

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from the site 'You Site Name'"; //letter subject
+	$to = 'spencer.holladay@gmail.com'; // Enter Your E-mail
+	$from = 'sholladay@gannett.com'; // Admin e-mail
+	$fromName = 'Your Company Name'; // Your company name
+	$charset = 'UTF-8';
+?>
